c0d60dde1fe684d6e7272a2e75866e821743e75d
Kenny Chan <kennychan1225@gmail.com>
2020-12-15 09:55:08 +0800

    first commit with load dataset

---
+++ common_config.py
@@ -0,0 +1,15 @@
+# common config
+
+__batch_size__ = 32
+__img_height__ = 180
+__img_width__ = 180
+
+__train_dir__ = 'archive/seg_train/seg_train/'
+__pred_dir__ = 'archive/seg_pred/seg_pred/'
+__test_dir__ = 'archive/seg_test/seg_test/'
+__buildings__ = 'buildings'
+__forest__ = 'forest'
+__glacier__ = 'glacier'
+__mountain__ = 'mountain'
+__sea__ = 'sea'
+__street__ = 'street'
---
+++ run.py
@@ -0,0 +1,4 @@
+import loadImage as load
+
+image_ds = load.load_train_ds()
+print(image_ds.class_names)
